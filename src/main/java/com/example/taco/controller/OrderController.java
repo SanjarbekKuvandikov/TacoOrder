64be2a88fff318af7d36f4f1f9fe36ad2a3b94a3
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-27 18:04:41 +0500

    bugs fixed in OrderController

--- src/main/java/com/example/taco/controller/OrderController.java
+++ src/main/java/com/example/taco/controller/OrderController.java
@@ -2,23 +2,26 @@ package com.example.taco.controller;
 
 import com.example.taco.data.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
+@Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
-    @ModelAttribute("tacoOrder")
-    public TacoOrder tacoOrder() {
-        return new TacoOrder();
+    @GetMapping("/current")
+    public String orderForm(){
+        return "orderFrom";
     }
 
-    @GetMapping("/current")
-    public String orderForm(@ModelAttribute("tacoOrder") TacoOrder tacoOrder){
-        return "orderFrom";
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder, SessionStatus status){
+        log.info("Order submitted = {}", tacoOrder);
+        status.setComplete();
+
+        return "redirect:/";
     }
 }
